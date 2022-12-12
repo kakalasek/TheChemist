f64f1b68eb9c6938c9f84996530254c49b04ca83
Typicek <carotkacz333@seznam.cz>
2022-12-12 23:00:11 +0100

    Extras

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -1,11 +1,24 @@
 package Main;
 
+import Screens.GameMenu;
+
 import javax.swing.JPanel;
-import java.awt.Color;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+    GameMenu gameMenu = new GameMenu();
 
+    //konstruktor
     public GamePanel(){
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.BLACK); //nastaveni pozadi
+        this.setFocusable(true); //nastaveni soustredeni
+    }
+
+    //metoda pro zobrazovani kontentu
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); // ??
+        Graphics2D g2 = (Graphics2D)g; //vytvoreni 2d grafiky
+
+        gameMenu.draw(g2);
     }
 }
